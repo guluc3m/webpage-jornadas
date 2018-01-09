d22fd1b0808d91bf521f41f377d982e4bfde0743
Daniel Rodriguez <drodriguez@oc8815134162.ibm.com>
2018-01-09 16:28:28 +0100

    cambios estilo y cosas de iconos

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,1 +1,36 @@
-console.log("prueba prueba")
+//Al rellenar los input text: emailpj1, emailpj2 y emailpj3 cambiar la opacidad de "iconpjX" correspondiente
+
+document.addEventListener("DOMContentLoaded", function(event) {
+	var mail1 = document.getElementById('emailpj1');
+	var mail2 = document.getElementById('emailpj2');
+	var mail3 = document.getElementById('emailpj3');
+	mail1.onchange = function(){
+		if (verifyEmail(mail1.value)) {
+			document.getElementById('iconpj1').style.opacity = 1;
+		} else {
+			document.getElementById('iconpj1').style.opacity = 0;
+		}
+	};
+	mail2.onchange = function(){
+		if (verifyEmail(mail2.value)) {
+			document.getElementById('iconpj2').style.opacity = 1;
+		} else {
+			document.getElementById('iconpj2').style.opacity = 0;
+		}
+	};
+	mail3.onchange = function(){
+		if (verifyEmail(mail3.value)) {
+			document.getElementById('iconpj3').style.opacity = 1;
+		} else {
+			document.getElementById('iconpj3').style.opacity = 0;
+		}
+	};
+});
+
+function verifyEmail(text){
+	var emailRegEx = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+     if (text.search(emailRegEx) != -1) {
+          return true
+     }
+     return false;
+}
