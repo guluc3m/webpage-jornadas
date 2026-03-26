ddec928d642666c7b94b171892a31753290c068f
Jorge Adrian Saghin Dudulea <zanajorgesaghin@gmail.com>
2026-03-27 00:34:17 +0100

    fix script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
-docker stop webpage-jornadas
+docker stop web-jornadas
+docker rm web-jornadas
 docker build -t webpage-jornadas .
 docker run -d -p 8069:80 --name web-jornadas webpage-jornadas:latest
