2265ae39f82da5fe30c9dd698ded85fa63eec346
Jorge Adrian Saghin Dudulea <zanajorgesaghin@gmail.com>
2026-03-26 17:43:19 +0100

    add reactivity
    que asco me da CSS, bombardeen torremolinos

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 docker stop webpage-jornadas
 docker build -t webpage-jornadas .
-docker run -d -p 8069:80 -h web-jornadas webpage-jornadas:latest
+docker run -d -p 8069:80 --name web-jornadas webpage-jornadas:latest
--- static/js/charlas.js
+++ static/js/charlas.js
@@ -4,15 +4,15 @@ const eventData = {
             id: 1,
             charlas: [
                 {
-                    titulo:    "AAAAAAAAAAAAAAAAAA",
-                    ponente:   "111111111111111111",
+                    titulo:    "Como ser tu propio jefe",
+                    ponente:   "Pepito de los Palotes",
                     horario:   "13:00 - 15:00",
                     aula:      "2.3.C05",
                     descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lobortis elementum nisi id volutpat. Nunc ut nulla libero. Duis nec nibh a ex efficitur vulputate. Nulla vitae turpis augue."
                 },
                 {
-                    titulo:    "BBBBBBBBBBBBBBBB",
-                    ponente:   "2222222222222222",
+                    titulo:    "Seguro quieres ser tu jefe?",
+                    ponente:   "Papote de los Pelitos",
                     horario:   "16:00 - 18:00",
                     aula:      "1.1.A01",
                     descripcion: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa quae ab illo inventore veritatis."
@@ -23,15 +23,15 @@ const eventData = {
             id: 2,
             charlas: [
                 {
-                    titulo:    "CCCCCCCCCCCCCCCCCC",
-                    ponente:   "333333333333333333",
+                    titulo:    "Sueltame del brazo, no quiero ser mi propio jefe",
+                    ponente:   "Alguien random",
                     horario:   "10:00 - 12:00",
                     aula:      "3.2.B10",
                     descripcion: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi."
                 },
                 {
-                    titulo:    "DDDDDDDDDDDDDDDDDDD",
-                    ponente:   "4444444444444444444",
+                    titulo:    "Voy a llamar a la policia",
+                    ponente:   "Seguramente otro random",
                     horario:   "13:30 - 15:30",
                     aula:      "2.1.C02",
                     descripcion: "Nam libero tempore cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus omnis voluptas assumenda est."
@@ -42,15 +42,15 @@ const eventData = {
             id: 3,
             charlas: [
                 {
-                    titulo:    "EEEEEEEEEEEEEEEEEEE",
-                    ponente:   "5555555555555555555",
+                    titulo:    "Que cojones estoy poniendo",
+                    ponente:   "Mike Wasowski",
                     horario:   "09:00 - 11:00",
                     aula:      "1.4.D03",
                     descripcion: "Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae."
                 },
                 {
-                    titulo:    "FFFFFFFFFFFFFFFFFFF",
-                    ponente:   "6666666666666666666",
+                    titulo:    "Crazy? I was crazy once",
+                    ponente:   "They locked me in a room",
                     horario:   "14:00 - 16:00",
                     aula:      "2.3.C05",
                     descripcion: "Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
@@ -61,15 +61,15 @@ const eventData = {
             id: 4,
             charlas: [
                 {
-                    titulo:    "GGGGGGGGGGGGGGGGGGGGG",
-                    ponente:   "777777777777777777777",
+                    titulo:    "A rubber room",
+                    ponente:   "A rubber room full of rats",
                     horario:   "11:00 - 13:00",
                     aula:      "4.1.E08",
                     descripcion: "Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur vel illum qui dolorem eum fugiat quo voluptas nulla pariatur."
                 },
                 {
-                    titulo:    "HHHHHHHHHHHHHHHHHHHHHH",
-                    ponente:   "8888888888888888888888",
+                    titulo:    "Rats made me crazy",
+                    ponente:   "Crazy? I was crazy once",
                     horario:   "15:00 - 17:00",
                     aula:      "3.3.B05",
                     descripcion: "Ut enim ad minima veniam quis nostrum exercitationem ullam corporis suscipit laboriosam nisi ut aliquid ex ea commodi consequatur quid est laborum."
