ca33bf9e64325be7f50b22255c1fb8cf5ee37851
Jorge Adrian Saghin Dudulea <zanajorgesaghin@gmail.com>
2026-04-07 14:23:39 +0200

    comentarios mas serios gente

--- static/js/charlas.js
+++ static/js/charlas.js
@@ -4,17 +4,17 @@ const eventData = {
             id: 1,
             charlas: [
                 {
-                    titulo:    "Como ser tu propio jefe",
-                    ponente:   "Pepito de los Palotes",
-                    horario:   "13:00 - 15:00",
-                    aula:      "2.3.C05",
+                    titulo:    "Charla1",
+                    ponente:   "Nombre1",
+                    horario:   "XX:00 - XX:00",
+                    aula:      "XX.XX.XX",
                     descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lobortis elementum nisi id volutpat. Nunc ut nulla libero. Duis nec nibh a ex efficitur vulputate. Nulla vitae turpis augue."
                 },
                 {
-                    titulo:    "Seguro quieres ser tu jefe?",
-                    ponente:   "Papote de los Pelitos",
-                    horario:   "16:00 - 18:00",
-                    aula:      "1.1.A01",
+                    titulo:    "Charla2",
+                    ponente:   "Nombre2",
+                    horario:   "XX:00 - XX:00",
+                    aula:      "XX.XX.XX",
                     descripcion: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa quae ab illo inventore veritatis."
                 }
             ]
@@ -23,17 +23,17 @@ const eventData = {
             id: 2,
             charlas: [
                 {
-                    titulo:    "Sueltame del brazo, no quiero ser mi propio jefe",
-                    ponente:   "Alguien random",
-                    horario:   "10:00 - 12:00",
-                    aula:      "3.2.B10",
+                    titulo:    "Charla3",
+                    ponente:   "Nombre3",
+                    horario:   "XX:00 - XX:00",
+                    aula:      "XX.XX.XX",
                     descripcion: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi."
                 },
                 {
-                    titulo:    "Voy a llamar a la policia",
-                    ponente:   "Seguramente otro random",
-                    horario:   "13:30 - 15:30",
-                    aula:      "2.1.C02",
+                    titulo:    "Charla4",
+                    ponente:   "Nombre4",
+                    horario:   "XX:00 - XX:00",
+                    aula:      "XX.XX.XX",
                     descripcion: "Nam libero tempore cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus omnis voluptas assumenda est."
                 }
             ]
@@ -42,17 +42,17 @@ const eventData = {
             id: 3,
             charlas: [
                 {
-                    titulo:    "Que cojones estoy poniendo",
-                    ponente:   "Mike Wasowski",
-                    horario:   "09:00 - 11:00",
-                    aula:      "1.4.D03",
+                    titulo:    "Charla5",
+                    ponente:   "Nombre5",
+                    horario:   "XX:00 - XX:00",
+                    aula:      "XX.XX.XX",
                     descripcion: "Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae."
                 },
                 {
-                    titulo:    "Crazy? I was crazy once",
-                    ponente:   "They locked me in a room",
-                    horario:   "14:00 - 16:00",
-                    aula:      "2.3.C05",
+                    titulo:    "Charla6",
+                    ponente:   "Nombre6",
+                    horario:   "XX:00 - XX:00",
+                    aula:      "XX.XX.XX",
                     descripcion: "Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
                 }
             ]
@@ -61,17 +61,17 @@ const eventData = {
             id: 4,
             charlas: [
                 {
-                    titulo:    "A rubber room",
-                    ponente:   "A rubber room full of rats",
-                    horario:   "11:00 - 13:00",
-                    aula:      "4.1.E08",
+                    titulo:    "Charla7",
+                    ponente:   "Nombre7",
+                    horario:   "XX:00 - XX:00",
+                    aula:      "XX.XX.XX",
                     descripcion: "Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur vel illum qui dolorem eum fugiat quo voluptas nulla pariatur."
                 },
                 {
-                    titulo:    "Rats made me crazy",
-                    ponente:   "Crazy? I was crazy once",
-                    horario:   "15:00 - 17:00",
-                    aula:      "3.3.B05",
+                    titulo:    "Charla8",
+                    ponente:   "Nombre8",
+                    horario:   "XX:00 - XX:00",
+                    aula:      "XX.XX.XX",
                     descripcion: "Ut enim ad minima veniam quis nostrum exercitationem ullam corporis suscipit laboriosam nisi ut aliquid ex ea commodi consequatur quid est laborum."
                 }
             ]
